5a25405f7deb169fc4ffaf4261912c32ccf4632c
Anushree <anushreedeshpande123@gmail.com>
2017-10-25 21:18:21 +0530

    login screen

---
+++ app/src/main/java/com/example/anushree/complaint_box/Complaint_Box.java
@@ -0,0 +1,13 @@
+package com.example.anushree.complaint_box;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Complaint_Box extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint__box);
+    }
+}
